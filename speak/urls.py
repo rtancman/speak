9cc763140e65ce65077c5518c3ff9388501cbbc0
Raffael Tancman <rtancman@gmail.com>
2016-01-23 11:12:02 -0200

    adiciona home view

--- speak/urls.py
+++ speak/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from speak.core.views import home
 
 urlpatterns = [
+    url(r'^$', home),
     url(r'^admin/', admin.site.urls),
 ]
